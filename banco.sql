7f49ece3018b0b701b592551eb43f816c91fbe62
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-10-24 13:21:17 -0300

    Criado a tabela em MySQL

--- banco.sql
+++ banco.sql
@@ -1,1 +1,9 @@
-CREATE DATABASE cadastro;
+#create database cadastro;
+create table pessoas (
+nome varchar(30),
+idade tinyint(),
+sexo char(1),
+nacionalidade varchar(30),
+altura float(),
+peso float()
+);
